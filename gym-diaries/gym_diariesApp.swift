1813e23c626704930621d891754ae287cb709d9e
Saurabh Jain <saurabhpjain6@gmail.com>
2023-01-22 18:04:35 -0500

    Initial commit for IOS application

---
+++ gym-diaries/gym_diariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  gym_diariesApp.swift
+//  gym-diaries
+//
+//  Created by Saurabh Jain on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct gym_diariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
